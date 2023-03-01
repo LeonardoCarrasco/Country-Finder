164019846ee5f16c20ef8263f78dde2d118fda3f
Leo <leo07carrasco@gmail.com>
2023-03-01 14:17:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import CountryData from './country-info/CountryData';
+
+const URL = "https://restcountries.com/v3.1/all";
+
+export default function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [filterCountry, setFilterCountry] = useState("");
+  const [show, setShow] = useState(false);
+  const [nation, setNation] = useState([]);
+
+  const handleChange = (e) => {
+    setFilterCountry(e.target.value);
+    e.target.value === "" ? setShow(false) : "";
+  };
+
+  const showCountry = (country) => {
+    setShow((prev) => !prev);
+    setNation(country);
+  };
+
+  useEffect(() => {
+
+    if(filterCountry != ""){
+      fetch(URL)
+      .then((res) => res.json())
+      .then((data) =>
+        setCountries(
+          data.filter((country) =>
+            country.name.common.toLowerCase().includes(filterCountry)
+          )
+        )
+      );
+    }
+
+  }, [filterCountry]);
+
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Search for a Country:</h1>
+        <input onChange={handleChange} value={filterCountry} />
+      </div>
+      <p>debug: {filterCountry}</p>
+
+      <div className='container-countries'>
+
+        {
+          filterCountry === "" ? (
+            <h3>Find country</h3>
+          ) : 
+          countries.length === 0 ? (
+            <h3>No countries found.</h3>
+          ) : countries.length > 10 ? (
+            <h3>Too many matches, please be more specific.</h3>
+          ) : countries.length < 10 && countries.length > 1 ? (
+            countries.map((country) => (
+              <div className='item' key={country.name.common}>
+                <h2>{country.name.common}</h2>
+                <button onClick={() => showCountry(country)}>SHOW</button>
+              </div>
+            ))
+          ) : countries.length === 1 ? <CountryData data={countries[0]}/> : ""}
+
+        {show ? <CountryData data={nation} /> : ""}
+
+      </div>
+    </div>
+  );
+}
---
+++ src/country-info/CountryData.jsx
@@ -0,0 +1,27 @@
+const CountryData = ({ data }) => {
+
+    return (
+      <div className="country">
+        {!data ? (
+          ""
+        ) : (
+          <>
+            <h1>{data.name.common}</h1>
+            <p>Capital: {data.capital[0]}</p>
+            <p>population: {data.population}</p>
+            <h2>Languages</h2>
+            <ul>
+              {Object.values(data.languages).map((lang) => (
+                <li key={lang}>{lang}</li>
+              ))}
+            </ul>
+            <div>
+              <img src={data.flags.png} alt="" />
+            </div>
+          </>
+        )}
+      </div>
+    );
+  };
+  
+  export default CountryData;
